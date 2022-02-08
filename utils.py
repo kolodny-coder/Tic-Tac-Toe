bbb4378fcaa31d320a3784ea92a00fcdd2a9a915
dankolodny <kolodnydan@gmail.com>
2022-02-08 09:11:52 +0200

    feat(board-display): improve board display

--- utils.py
+++ utils.py
@@ -1,10 +1,16 @@
 def display_board(board):
     print('\n' * 1)
-
+    print('            |   |')
     print('          ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('          ' +'---  |  ---  |  ---')
+    print('            |   |')
+    print('        --------------')
+    print('            |   |')
+
     print('          ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('            |   |')
+    print('        --------------')
     print('          ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('            |   |')
 
 
 def place_marker(board, mark, position):
@@ -105,4 +111,4 @@ def minmax(board, depth, is_maximazing):
 
         return best_score
 
-display_board([' ']*10)
+display_board(['#', 'x', 'x', 'o', 'o', 'o', 'o','o', 'x', 'x'])
